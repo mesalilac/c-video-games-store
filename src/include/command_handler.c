00605ced516ee73fec669c78fa018c4c4f9ecdad
abdalrzag eisa <bmjfdrh@gmail.com>
2024-02-16 13:13:04 +0300

    add new commands

--- src/include/command_handler.c
+++ src/include/command_handler.c
@@ -9,9 +9,22 @@ void command_handler(char *input, int *quit_shell) {
         if (strcmp(input, "help") == 0) {
             help_print("help", "Print this message");
             help_print("exit", "Exit the shell");
+            help_print("list", "List all games");
+            help_print("add", "add a new game");
+            help_print("change", "change name or price of a game");
+            help_print("remove", "remove a game");
         }
+
         else if (strcmp(input, "exit") == 0) {
             printf("Exiting shell...\n");
             *quit_shell = 1;
         }
+
+        else if (strcmp(input, "list") == 0) {}
+
+        else if (strcmp(input, "add") == 0) {}
+
+        else if (strcmp(input, "change") == 0) {}
+
+        else if (strcmp(input, "remove") == 0) {}
 }
